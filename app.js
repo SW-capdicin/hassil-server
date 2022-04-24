d4bacadcda1fe7e49829aeb764f71b1a83746c35
yongjin <dydwls0669@gmail.com>
2022-04-24 11:30:26 +0900

    Setup prettier/eslint

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
-const path = require("path");
-const session = require("express-session");
-const dotenv = require("dotenv");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const dotenv = require('dotenv');
 dotenv.config();
-const indexRouter = require("./routes/index");
-const { sequelize } = require("./models");
+
+const { sequelize } = require('./models');
 
 const app = express();
-app.set("port", process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8080);
 
 sequelize
   .sync({ force: false })
   .then(() => {
-    console.log("DB 연결 성공");
+    console.log('DB 연결 성공');
   })
   .catch((err) => {
     console.error(err);
   });
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -33,10 +33,10 @@ app.use(
       httpOnly: true,
       secure: false,
     },
-  })
+  }),
 );
 
-app.use("/", indexRouter);
+app.use('/', require('./routes/index'));
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -44,13 +44,13 @@ app.use((req, res, next) => {
   next(error);
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
-app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기 중");
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
 });

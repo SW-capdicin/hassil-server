6a4e81b6a75c457c94b54413ba4942c9dbdf7e4f
coderclcl <gattenmaster@gmail.com>
2022-04-24 13:21:42 +0900

    config.js .env 변경

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 module.exports = class User extends Sequelize.Model {
   static init(sequelize) {
@@ -11,19 +11,19 @@ module.exports = class User extends Sequelize.Model {
           unique: true,
         },
         email: {
-          type: "varchar(45)",
+          type: 'varchar(45)',
           allowNull: false,
         },
         pid: {
-          type: "varchar(45)",
+          type: 'varchar(45)',
           allowNull: false,
         },
         nickname: {
-          type: "varchar(45)",
+          type: 'varchar(45)',
           allowNull: false,
         },
         phone_number: {
-          type: "varchar(45)",
+          type: 'varchar(45)',
           allowNull: false,
         },
         point: {
@@ -35,59 +35,59 @@ module.exports = class User extends Sequelize.Model {
           allowNull: false,
         },
         name: {
-          type: "varchar(45)",
+          type: 'varchar(45)',
           allowNull: false,
         },
         bank_name: {
-          type: "varchar(45)",
+          type: 'varchar(45)',
           allowNull: false,
         },
         bank_account: {
-          type: "varchar(45)",
+          type: 'varchar(45)',
           allowNull: false,
         },
         src: {
-          type: "varchar(255)",
-          allowNull: false,
+          type: 'varchar(255)',
+          allowNull: true,
         },
       },
       {
         sequelize,
         timestamps: true,
         underscored: true,
-        modelName: "User",
-        tableName: "user",
+        modelName: 'User',
+        tableName: 'user',
         paranoid: true,
-        charset: "utf8",
-        collate: "utf8_general_ci",
-      }
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      },
     );
   }
 
   static associate(db) {
     db.User.hasMany(db.StudyCafe, {
-      foreignKey: "user_id",
+      foreignKey: 'user_id',
     });
     db.User.hasMany(db.Review, {
-      foreignKey: "user_id",
+      foreignKey: 'user_id',
     });
     db.User.hasMany(db.Notice, {
-      foreignKey: "user_id",
+      foreignKey: 'user_id',
     });
     db.User.hasMany(db.Question, {
-      foreignKey: "user_id",
+      foreignKey: 'user_id',
     });
     db.User.hasMany(db.Answer, {
-      foreignKey: "user_id",
+      foreignKey: 'user_id',
     });
     db.User.hasMany(db.StudyMember, {
-      foreignKey: "user_id",
+      foreignKey: 'user_id',
     });
     db.User.hasMany(db.Comment, {
-      foreignKey: "user_id",
+      foreignKey: 'user_id',
     });
     db.User.hasMany(db.PointHistory, {
-      foreignKey: "user_id",
+      foreignKey: 'user_id',
     });
   }
 };

f5092ebe5003393f41d5546496fa6b0c20935171
유주현 <gus6359@gmail.com>
2022-04-30 03:09:24 +0900

    setting: deploy config fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   apps : [{
+    name: 'hassil-dev',
     script: 'app.js',
     watch: true,
-    ignore_watch : ["node_modules"],
+    ignore_watch : ['node_modules'],
+    env: {
+      STAGE: 'dev',
+      NODE_ENV: 'development'
+    }
   }]
 };

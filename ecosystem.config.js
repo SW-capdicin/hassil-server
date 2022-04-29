8db8488289716ecadd806dfc00a5b01a6b0fd109
유주현 <gus6359@gmail.com>
2022-04-30 04:44:51 +0900

    setting: deploy config & package-lock

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   apps : [{
     name: 'hassil-dev',
-    script: 'app.js',
+    script: './app.js',
     watch: true,
-    ignore_watch : ['node_modules'],
     env: {
       STAGE: 'dev',
       NODE_ENV: 'development'

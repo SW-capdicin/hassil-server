e700fd08b3cb36d040fadf540121f233604c0fc4
유주현 <gus6359@gmail.com>
2022-05-11 22:19:51 +0900

    rollback

--- config/googleStrategy.js
+++ config/googleStrategy.js
@@ -1,9 +1,4 @@
-const envConfig = stage => ({ path: `../.env${stage ? `.${stage}` : ''}` })
-
-require('dotenv').config(envConfig(process.env.NODE_ENV));
-
-console.log(process.env.NODE_ENV);
-console.log(process.env.GOOGLE_CALLBACK_URL);
+require('dotenv').config();
 
 module.exports = {
   clientID: process.env.GOOGLE_CLIENT_ID,

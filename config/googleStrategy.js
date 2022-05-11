79837d510b8eaf84bfccece434d07ba4a37d7445
유주현 <gus6359@gmail.com>
2022-05-11 22:21:41 +0900

    get log

--- config/googleStrategy.js
+++ config/googleStrategy.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+console.log(process.env.GOOGLE_CLIENT_ID);
+console.log(process.env.GOOGGOOGLE_CALLBACK_URLLE_CLIENT_ID);
+
 module.exports = {
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,

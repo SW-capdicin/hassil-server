cdd57f9ad89bf09cfed05ff314b0a1c29c9d3ad5
유주현 <gus6359@gmail.com>
2022-05-11 22:24:13 +0900

    rm stage & env value

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,6 @@ module.exports = {
   apps : [{
     name: 'hassil-prod',
     script: './app.js',
-    watch: true,
-    env_production: {
-      STAGE: 'prod',
-      NODE_ENV: 'production'
-    }
+    watch: true
   }]
 };

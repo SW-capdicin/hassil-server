69da975557e7590b7f5f337337a384f3abad7abc
유주현 <gus6359@gmail.com>
2022-05-11 21:49:30 +0900

    fix: bug

--- config/googleStrategy.js
+++ config/googleStrategy.js
@@ -1,6 +1,4 @@
-const path = require('path');
-
-const envConfig = stage => ({ path: path.join(__dirname, `.env${stage ? `.${stage}` : ''}`) })
+const envConfig = stage => `../.env${stage ? `.${stage}` : ''}`
 
 require('dotenv').config(envConfig(process.env.NODE_ENV))
 
